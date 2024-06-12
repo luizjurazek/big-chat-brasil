074c6a2189a8ce70afc944c086eec00a4d5ff53a
Luiz Jurazek <luizjurazek@gmail.com>
2024-06-12 20:09:41 -0300

    feat: create connection with db

---
+++ src/config/connection.ts
@@ -0,0 +1,18 @@
+import { Dialect, Sequelize } from "sequelize";
+import { config as dotenvConfig } from "dotenv";
+dotenvConfig();
+
+const DB_USER: string = process.env.DB_USER || "";
+const DB_PASSWORD: string = process.env.DB_PASSWORD || "";
+const DB_HOST: string = process.env.DB_HOST || "";
+const DB_PORT: number = parseInt(process.env.DB_PORT || '5432', 10);
+const DB_NAME: string = process.env.DB_NAME || "";
+const DB_DIALECT: Dialect = process.env.DB_DIALECT as Dialect;
+
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  dialect: DB_DIALECT,
+  host: DB_HOST,
+  port: DB_PORT
+});
+
+export { sequelize };
